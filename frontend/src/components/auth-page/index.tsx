223bd1bf3b74287e1e229f80f5b6a6978e407e59
Satriauno <68578408+Satriauno@users.noreply.github.com>
2025-10-31 11:56:47 +0700

    BE

--- frontend/src/components/auth-page/index.tsx
+++ frontend/src/components/auth-page/index.tsx
@@ -3,12 +3,15 @@ import { AuthPage as AuthPageBase } from "@refinedev/antd";
 import type { AuthPageProps } from "@refinedev/core";
 
 export const AuthPage = (props: AuthPageProps) => {
-  return (
+  return ( 
     <AuthPageBase
-      {...props}
+      type="login"
+      title="Dragonfruit Grading"
       formProps={{
-        initialValues: { email: "demo@refine.dev", password: "demodemo" },
+        initialValues: { email: "demo@example.com", password: "demodmeo" },
+        // onSubmit: async (values) => { /* custom login */ }
       }}
+      wrapperProps={{ style: { backgroundImage: "url('/bg.jpg')" } }}
     />
   );
 };
